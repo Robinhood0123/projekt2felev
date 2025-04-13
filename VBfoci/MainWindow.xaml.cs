f4a8d61a123125cb5794f26ef040e5251fe4111c
Norbiacs <93657722+Norbiacs@users.noreply.github.com>
2025-04-13 09:42:29 +0200

    beolvas-kiir-megszamol

--- VBfoci/MainWindow.xaml.cs
+++ VBfoci/MainWindow.xaml.cs
@@ -40,12 +40,37 @@ namespace VBfoci
 
         private void Beolvasas_Click(object sender, RoutedEventArgs e)
         {
+            string fajlNev = txtFajlNev.Text;
 
+            resztvevok.Clear();
+            listEredmenyek.Items.Clear();
+
+            var sorok = File.ReadAllLines(fajlNev).Skip(1);
+
+            foreach (string sor in sorok)
+            {
+                Resztvevo uj = new Resztvevo(sor);
+                resztvevok.Add(uj);
+
+                string szoveg = uj.Ev + " - " + uj.Orszag + " (" + uj.Helyezes + ".) - " + uj.Helyszin;
+                listEredmenyek.Items.Add(szoveg);
+            }
         }
 
         private void Kiiras_Click(object sender, RoutedEventArgs e)
         {
+            string fajlNev = txtKiirasFajlba.Text;
 
+            StreamWriter iro = new StreamWriter(fajlNev);
+
+            foreach (Resztvevo adat in resztvevok)
+            {
+                string sor = adat.Orszag + "; " + adat.Helyezes + "; " + adat.Ev + "; " + adat.Helyszin;
+                iro.WriteLine(sor);
+            }
+
+            iro.Close();
+            MessageBox.Show("A kiírás sikeresen befejeződött!");
         }
 
         private void Kereses_Click(object sender, RoutedEventArgs e)
@@ -55,6 +80,37 @@ namespace VBfoci
 
         private void Megszamolas_Click(object sender, RoutedEventArgs e)
         {
+            int osszeg = 0;
+            Dictionary<string, int> helyszinSzam = new Dictionary<string, int>();
+
+            foreach (Resztvevo adat in resztvevok)
+            {
+                osszeg += adat.Helyezes;
+
+                if (helyszinSzam.ContainsKey(adat.Helyszin))
+                {
+                    helyszinSzam[adat.Helyszin]++;
+                }
+                else
+                {
+                    helyszinSzam.Add(adat.Helyszin, 1);
+                }
+            }
+
+            double atlag = (double)osszeg / resztvevok.Count;
+
+            string leggyakoribb = "";
+            int maxdb = 0;
+            foreach (var par in helyszinSzam)
+            {
+                if (par.Value > maxdb)
+                {
+                    maxdb = par.Value;
+                    leggyakoribb = par.Key;
+                }
+            }
+
+            MessageBox.Show("Átlagos helyezés: " + atlag.ToString() +"\nLeggyakoribb helyszín: " + leggyakoribb);
 
         }
 
